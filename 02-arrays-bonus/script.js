39615d2a3eff773b552ed95df83ef5528b032441
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-17 16:54:44 +0200

    esbonus2

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -14,8 +14,12 @@ const reversedTeachers = teachers.reverse()
 console.log(teachers)
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
-
+const longNames = []
+for(i=0; i<teachers.length; i++){
+  if (teachers[i].length>=5)
+    longNames.push(teachers[i])
+}
+console.log(longNames)
 // 3. Rimuovi 'Ed' dall'array teachers
 
 // 4. Verifica se 'Fabio' è presente nell'array teachers
