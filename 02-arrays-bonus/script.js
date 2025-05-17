b7326861f31795f184f9a146f0d6a7bfa85e3a77
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-17 17:30:03 +0200

    esbonus4

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -25,8 +25,13 @@ teachers.splice(5, 1)
 console.log(teachers)
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
-
-
+const daTrovare = 'Fabio'
+let presente = false
+for (i=0; i<teachers.length; i++){
+if (teachers[i] === daTrovare){
+  presente = true}
+}
+const isFabioPresent = presente;
+console.log(isFabioPresent)
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
