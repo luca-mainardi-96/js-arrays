2f24a27941410f183ed8fc787e2a31f37dd35907
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-05-18 13:00:15 +0200

    correzione

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,8 +10,14 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = teachers.reverse()
-console.log(teachers)
+// const reversedTeachers = teachers.toReversed()
+const reversedTeachers = []
+for (let i = 0; i < teachers.length; i++) {
+  const index = teachers.length -1 -i
+  const element = teachers [index]
+  reversedTeachers.push(element)
+}
+
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
 const longNames = []
@@ -19,12 +25,18 @@ for(i=0; i<teachers.length; i++){
   if (teachers[i].length>=5)
     longNames.push(teachers[i])
 }
-console.log(longNames)
+//longNames = teachers.filter(teachers[i]=>teachers[i].length >=5)
+
 // 3. Rimuovi 'Ed' dall'array teachers
-teachers.splice(5, 1)
-console.log(teachers)
+const indexEd = teachers.indexOf("Ed")
+if (indexEd !== -1){
+  teachers.splice(indexEd, 1)
+}
+// teachers.splice(5, 1)
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
+//let isFabioPresent = teachers.includes('Fabio')
 const daTrovare = 'Fabio'
 let presente = false
 for (i=0; i<teachers.length; i++){
@@ -33,7 +45,13 @@ if (teachers[i] === daTrovare){
 }
 const isFabioPresent = presente;
 console.log(isFabioPresent)
+
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-// const teachersString = teachers.join(",");
-const teachersString = null
+// const teachersString = teachers.join(", ");
+let teachersString = teachers[0]
+separatore = ", "
+for (i=1; i<teachers.length; i++){
+  const name = teachers[i]
+  teachersString += separatore + name
+}
 console.log(teachersString)
